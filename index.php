0a92a4f40427da32abe2d304ef4cfaf98c51e892
testery20041 <testery20041@yahoo.com>
2015-05-14 11:15:37 +0300

    Revert "c1"
    This reverts commit 68cc5a0dff1c6a58255b86dbc89868b09d87b581.

--- index.php
+++ index.php
@@ -13,6 +13,3 @@ $customer = new Customers();
 echo $customer->getFullName();
 echo '<br/>';
 echo $customer->getFullName_r();
-
-$date = DateTime::createFromFormat('j-M-Y', '15-Feb-2009');
-echo '<br/>'.$date->format('Y-m-d');
