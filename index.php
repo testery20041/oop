68cc5a0dff1c6a58255b86dbc89868b09d87b581
testery20041 <testery20041@yahoo.com>
2015-05-14 11:15:03 +0300

    c1

--- index.php
+++ index.php
@@ -13,3 +13,6 @@ $customer = new Customers();
 echo $customer->getFullName();
 echo '<br/>';
 echo $customer->getFullName_r();
+
+$date = DateTime::createFromFormat('j-M-Y', '15-Feb-2009');
+echo '<br/>'.$date->format('Y-m-d');
